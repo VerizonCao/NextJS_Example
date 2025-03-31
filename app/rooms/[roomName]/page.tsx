94010e6d46057e4e2fa2a3887beeda8bacc224b3
root <root@DESKTOP-3KEC1A7>
2025-03-30 22:56:46 -0700

    use sync

--- app/rooms/[roomName]/page.tsx
+++ app/rooms/[roomName]/page.tsx
@@ -2,35 +2,6 @@ import * as React from 'react';
 import { PageClientImpl } from './PageClientImpl';
 import { isVideoCodec } from '@/lib/types';
 
-// export default async function Page({
-//   params,
-//   searchParams,
-// }: {
-//   params: { roomName: string };
-//   searchParams: {
-//     // FIXME: We should not allow values for regions if in playground mode.
-//     region?: string;
-//     hq?: string;
-//     codec?: string;
-//   };
-// }) {
-
-//   // damn wait here 
-//   const searchParamsWaited = await searchParams;
-//   const paramsWaited = await params;
-
-//   const codec =
-//     typeof searchParamsWaited.codec === 'string' && isVideoCodec(searchParamsWaited.codec)
-//       ? searchParamsWaited.codec
-//       : 'vp9';
-//   const hq = searchParamsWaited.hq === 'true' ? true : false;
-
-
-//   return (
-//     <PageClientImpl roomName={paramsWaited.roomName} region={searchParamsWaited.region} hq={hq} codec={codec} />
-//   );
-// }
-
 
 export default async function Page({
   params,
