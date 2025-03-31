bb7c95454643e7822685b5e6a02f0aa7b2c95b92
root <root@DESKTOP-3KEC1A7>
2025-03-30 22:11:04 -0700

    fix async?

--- app/rooms/[roomName]/page.tsx
+++ app/rooms/[roomName]/page.tsx
@@ -2,7 +2,37 @@ import * as React from 'react';
 import { PageClientImpl } from './PageClientImpl';
 import { isVideoCodec } from '@/lib/types';
 
-export default async function Page({
+// export default async function Page({
+//   params,
+//   searchParams,
+// }: {
+//   params: { roomName: string };
+//   searchParams: {
+//     // FIXME: We should not allow values for regions if in playground mode.
+//     region?: string;
+//     hq?: string;
+//     codec?: string;
+//   };
+// }) {
+
+//   // damn wait here 
+//   const searchParamsWaited = await searchParams;
+//   const paramsWaited = await params;
+
+//   const codec =
+//     typeof searchParamsWaited.codec === 'string' && isVideoCodec(searchParamsWaited.codec)
+//       ? searchParamsWaited.codec
+//       : 'vp9';
+//   const hq = searchParamsWaited.hq === 'true' ? true : false;
+
+
+//   return (
+//     <PageClientImpl roomName={paramsWaited.roomName} region={searchParamsWaited.region} hq={hq} codec={codec} />
+//   );
+// }
+
+
+export default function Page({
   params,
   searchParams,
 }: {
@@ -15,30 +45,17 @@ export default async function Page({
   };
 }) {
 
-  // damn wait here 
-  const searchParamsWaited = await searchParams;
-  const paramsWaited = await params;
-
-
-  // const codec =
-  //   typeof searchParams.codec === 'string' && isVideoCodec(searchParams.codec)
-  //     ? searchParams.codec
-  //     : 'vp9';
-  // const hq = searchParams.hq === 'true' ? true : false;
-
 
   const codec =
-    typeof searchParamsWaited.codec === 'string' && isVideoCodec(searchParamsWaited.codec)
-      ? searchParamsWaited.codec
+    typeof searchParams.codec === 'string' && isVideoCodec(searchParams.codec)
+      ? searchParams.codec
       : 'vp9';
-  const hq = searchParamsWaited.hq === 'true' ? true : false;
+  const hq = searchParams.hq === 'true' ? true : false;
 
 
-  // return (
-  //   <PageClientImpl roomName={params.roomName} region={searchParams.region} hq={hq} codec={codec} />
-  // );
 
   return (
-    <PageClientImpl roomName={paramsWaited.roomName} region={searchParamsWaited.region} hq={hq} codec={codec} />
+    <PageClientImpl roomName={params.roomName} region={searchParams.region} hq={hq} codec={codec} />
   );
+
 }
